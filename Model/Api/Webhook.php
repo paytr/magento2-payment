1df431508a08aca9c8e8085ee58661e0ebc25d2d
paytr-onur <onur.nuray@paytr.com>
2025-10-17 17:20:08 +0300

    fix OK problem

--- Model/Api/Webhook.php
+++ Model/Api/Webhook.php
@@ -32,33 +32,34 @@ class Webhook
     /**
      * Webhook constructor.
      *
-     * @param OrderFactory                   $orderFactory
-     * @param Context                        $context
-     * @param TransactionBuilder             $tb
+     * @param OrderFactory $orderFactory
+     * @param Context $context
+     * @param TransactionBuilder $tb
      * @param TransactionRepositoryInterface $transactionRepository
-     * @param Request                        $request
-     * @param PaytrHelper                    $paytrHelper
-     * @param OrderSender                    $orderSender
-     * @param LoggerInterface                $logger
+     * @param Request $request
+     * @param PaytrHelper $paytrHelper
+     * @param OrderSender $orderSender
+     * @param LoggerInterface $logger
      */
     public function __construct(
-        OrderFactory $orderFactory,
-        Context $context,
-        TransactionBuilder $tb,
+        OrderFactory                   $orderFactory,
+        Context                        $context,
+        TransactionBuilder             $tb,
         TransactionRepositoryInterface $transactionRepository,
-        Request $request,
-        PaytrHelper $paytrHelper,
-        OrderSender $orderSender,
-        LoggerInterface $logger
-    ) {
-        $this->orderFactory             = $orderFactory;
-        $this->config                    = $context->getScopeConfig();
-        $this->transactionBuilder       = $tb;
-        $this->transactionRepository    = $transactionRepository;
-        $this->request                  = $request;
-        $this->paytrHelper              = $paytrHelper;
-        $this->orderSender              = $orderSender;
-        $this->logger                   = $logger;
+        Request                        $request,
+        PaytrHelper                    $paytrHelper,
+        OrderSender                    $orderSender,
+        LoggerInterface                $logger
+    )
+    {
+        $this->orderFactory = $orderFactory;
+        $this->config = $context->getScopeConfig();
+        $this->transactionBuilder = $tb;
+        $this->transactionRepository = $transactionRepository;
+        $this->request = $request;
+        $this->paytrHelper = $paytrHelper;
+        $this->orderSender = $orderSender;
+        $this->logger = $logger;
     }
 
     /**
@@ -79,11 +80,11 @@ class Webhook
     public function getSuccessResponse($response)
     {
         if ($this->validateHash($response, $response['hash'])) {
-            $order_id   = $this->normalizeMerchantOid($response['merchant_oid']);
-            $order      = $this->orderFactory->create()->load($order_id);
+            $order_id = $this->normalizeMerchantOid($response['merchant_oid']);
+            $order = $this->orderFactory->create()->load($order_id);
             return $this->addTransactionToOrder($order, $response);
         } else {
-            return 'PAYTR notification failed: bad hash';
+            return $this->returnText('PAYTR notification failed: bad hash');
         }
     }
 
@@ -94,20 +95,20 @@ class Webhook
     public function getFailedResponse($response)
     {
         if ($this->validateHash($response, $response['hash'])) {
-            $order_id   = $this->normalizeMerchantOid($response['merchant_oid']);
-            $order      = $this->orderFactory->create()->load($order_id);
-            if($order->getState() == Order::STATE_PENDING_PAYMENT ||
+            $order_id = $this->normalizeMerchantOid($response['merchant_oid']);
+            $order = $this->orderFactory->create()->load($order_id);
+            if ($order->getState() == Order::STATE_PENDING_PAYMENT ||
                 $order->getState() == Order::STATE_NEW) {
-              $order->addStatusHistoryComment($response['failed_reason_msg']);
-              $order->cancel();
-              $order->setState(Order::STATE_CANCELED);
-              $order->setStatus("canceled");
-              $order->save();
-              return 'OK';
+                $order->addStatusHistoryComment($response['failed_reason_msg']);
+                $order->cancel();
+                $order->setState(Order::STATE_CANCELED);
+                $order->setStatus("canceled");
+                $order->save();
+                return $this->returnText('OK');
             }
-            return 'OK';
+            return $this->returnText('OK');
         } else {
-            return 'PAYTR notification failed: bad hash';
+            return $this->returnText('PAYTR notification failed: bad hash');
         }
     }
 
@@ -153,7 +154,7 @@ class Webhook
     public function addTransactionToOrder($order, $response)
     {
         if ($order->getState()) {
-            if($order->getState() == Order::STATE_PENDING_PAYMENT ||
+            if ($order->getState() == Order::STATE_PENDING_PAYMENT ||
                 $order->getState() == Order::STATE_NEW ||
                 $order->getState() == Order::STATE_CANCELED) {
                 $order->setState(Order::STATE_PROCESSING, true);
@@ -166,7 +167,7 @@ class Webhook
                     ->setOrder($order)
                     ->setTransactionId($response['merchant_oid'])
                     ->setAdditionalInformation(
-                        [Transaction::RAW_DETAILS => (array) $response]
+                        [Transaction::RAW_DETAILS => (array)$response]
                     )
                     ->setFailSafe(true)
                     ->build(Transaction::TYPE_ORDER);
@@ -182,11 +183,11 @@ class Webhook
                 } catch (\Throwable $e) {
                     $this->logger->critical($e);
                 }
-                return 'OK';
+                return $this->returnText('OK');
             }
-            return 'OK';
+            return $this->returnText('OK');
         }
-        return 'HATA: Sipariş durumu tamamlanmadı. Tekrar deneniyor.';
+        return $this->returnText('HATA: Sipariş durumu tamamlanmadı. Tekrar deneniyor.');
     }
 
     /**
@@ -196,11 +197,11 @@ class Webhook
      */
     public function customNote($response, $order)
     {
-        $currency               = $this->orderFactory->create()->load($order->getRealOrderId());
-        $currency               = $currency->getOrderCurrency()->getId();
-        $maturity_difference    = 'Vade Farkı: ' . (round(($response['total_amount'] - $response['payment_amount']) / 100)) . ' ' . $currency . '<br>';
-        $total_amount           = number_format(($response['total_amount'] / 100), 2, '.', '.');
-        $amount                 = number_format(($response['payment_amount'] / 100), 2, '.', '.');
+        $currency = $this->orderFactory->create()->load($order->getRealOrderId());
+        $currency = $currency->getOrderCurrency()->getId();
+        $maturity_difference = 'Vade Farkı: ' . (round(($response['total_amount'] - $response['payment_amount']) / 100)) . ' ' . $currency . '<br>';
+        $total_amount = number_format(($response['total_amount'] / 100), 2, '.', '.');
+        $amount = number_format(($response['payment_amount'] / 100), 2, '.', '.');
         $note = '<b>' . __('PAYTR NOTICE - Payment Accepted') . '</b><br>';
         $note .= __('Total Paid') . ': ' . $total_amount . ' ' . $currency . '<br>';
         $note .= __('Paid') . ': ' . $amount . ' ' . $currency . '<br>';
@@ -208,5 +209,11 @@ class Webhook
         $note .= __('Installment Count') . ': ' . ($response['installment_count'] === '1' ? 'Tek Çekim' : $response['installment_count']) . '<br>';
         $note .= __('PayTR Order Number') . ': <a href="https://www.paytr.com/magaza/islemler?merchant_oid=' . $response['merchant_oid'] . '" target="_blank">' . $response['merchant_oid'] . '</a><br>';
         return $note;
+    }
+
+    public function returnText($text)
+    {
+        echo $text;
+        die();
     }
 }
